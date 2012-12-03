f0498146a1bd4f7f7d46972cbf0e8c7229486ad5
Mike Ballan <info@mikeballan.co.uk>
2012-12-03 22:58:50 +0000

    New menu
    Added menu and styled up example pages

---
+++ includes/amazium.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){ 		
+	$(".navigation-btn").click(function(){
+		$(".navigation").slideToggle('slow');
+	});
+	
+	$(".quote").click(function(){
+		$(".quote-form").slideToggle('slow');
+	});
+}); 
