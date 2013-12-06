8a521f1f28faab0c588eecfe2445a01a7a6fd621
Mike Ballan <info@mikeballan.co.uk>
2013-12-06 16:48:03 +0000

    Adds support for nysmnyd lables

--- js/amazium.js
+++ js/amazium.js
@@ -1,17 +1,27 @@
-/* Updated November 2012 by Kris Rybak */
-    $(document).ready(function(){
-        $('.navigation-btn').click(function(){
-            $('.navigation').slideToggle('slow');
-        });
-        $(window).resize(function(){
-            var currentWidth = $(window).width();
-            if (currentWidth >= 768) {
-                $('.navigation').show();
-                $('.navigation').css("display","");
-            } else {
-                if( $('.navigation').css("display") == 'none' ){
-                    $('.navigation').hide();
-                }
-            }
-        });
+$(document).ready(function(){
+    /* Updated November 2012 by Kris Rybak */
+    $('.navigation-btn').click(function(){
+        $('.navigation').slideToggle('slow');
     });
+    $(window).resize(function(){
+        var currentWidth = $(window).width();
+        if (currentWidth >= 768) {
+            $('.navigation').show();
+            $('.navigation').css("display","");
+        } else {
+            if( $('.navigation').css("display") == 'none' ){
+                $('.navigation').hide();
+            }
+        }
+    });
+    /* Created November 2013 by Shahrukh Omar */
+    $('#nysmnyd').on('keyup', 'input,textarea', function() {
+        var $this = $(this)
+        var $fs   = $this.parents('fieldset')
+        if ($fs.length && $this.val().length) {
+            $fs.addClass('show-all')
+        } else {
+            $fs.removeClass('show-all')
+        }
+    })
+});
