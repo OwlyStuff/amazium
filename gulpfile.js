69fd17c26676ba77dc4352a11f059b92df1245c0
Chris Sedlmayr <chris@sedlmayr.co.uk>
2016-05-18 16:55:12 +0100

    Stop gulp from hiding errors during compilation

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var sass = require('gulp-sass');
 
 gulp.task('compile', function () {
   return gulp.src('./scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/'));
 });
 
