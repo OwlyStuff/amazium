31a084374a0c6c82c34608cc9eb26bb166d37a6a
Chris Sedlmayr <chris@sedlmayr.co.uk>
2016-05-20 14:42:22 +0100

    [Build] Adds CSS clean to the build pipeline, reduces filesize of built css by nearly 20K

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('compile', function () {
   return gulp.src('./scss/*.scss')
     .pipe(sass())
+    .pipe(cleanCSS())
     .pipe(gulp.dest('./dist/'));
 });
 
